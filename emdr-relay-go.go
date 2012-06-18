cf72b3b983c60de5f2f17dbaa51df4fdd2fd79e0
Greg Taylor <gtaylor@duointeractive.com>
2012-06-17 23:23:16 -0500

    Adding extra error handling so we can figure out what's leading to the memory hogging.

--- emdr-relay-go.go
+++ emdr-relay-go.go
@@ -1,51 +1,64 @@
 package main
 
 import (
-        "fmt"
-        zmq "github.com/alecthomas/gozmq"
-        "github.com/bradfitz/gomemcache/memcache"
-        "hash"
-        "hash/fnv"
+	"fmt"
+	zmq "github.com/alecthomas/gozmq"
+	"github.com/bradfitz/gomemcache/memcache"
+	"hash"
+	"hash/fnv"
 )
 
 func main() {
-        mc := memcache.New("127.0.0.1:11211")
+	mc := memcache.New("127.0.0.1:11211")
 
-        context, _ := zmq.NewContext()
+	context, _ := zmq.NewContext()
 
-        receiver, _ := context.NewSocket(zmq.SUB)
-        receiver.SetSockOptString(zmq.SUBSCRIBE, "")
-        receiver.Connect("tcp://master.eve-emdr.com:8050")
-        receiver.Connect("tcp://secondary.eve-emdr.com:8050")
+	receiver, _ := context.NewSocket(zmq.SUB)
+	receiver.SetSockOptString(zmq.SUBSCRIBE, "")
+	receiver.Connect("tcp://master.eve-emdr.com:8050")
+	receiver.Connect("tcp://secondary.eve-emdr.com:8050")
+	//receiver.Connect("tcp://relay-us-central-1.eve-emdr.com:8050")
 
-        sender, _ := context.NewSocket(zmq.PUB)
-        sender.Bind("tcp://0.0.0.0:8050")
+	sender, _ := context.NewSocket(zmq.PUB)
+	sender.Bind("tcp://0.0.0.0:8050")
 
-        println("Listening on port 8050...")
+	println("Listening on port 8050...")
 
-        for {
-                msg, _ := receiver.Recv(0)
+	for {
+		msg, zmq_err := receiver.Recv(0)
 
-                var h hash.Hash = fnv.New32()
-                h.Write(msg)
+		if zmq_err != nil {
+			println("RECV ERROR:", zmq_err.Error())
+		}
 
-                checksum := h.Sum([]byte{})
-                var checksum_str string = fmt.Sprintf("%x", checksum)
+		var h hash.Hash = fnv.New32()
+		h.Write(msg)
 
-                _, err := mc.Get(checksum_str)
+		checksum := h.Sum([]byte{})
+		var checksum_str string = fmt.Sprintf("%x", checksum)
 
-                mc.Set(&memcache.Item{Key: checksum_str, Value: []byte{1}, Expiration: 300})
+		_, mc_err := mc.Get(checksum_str)
 
-                if err == memcache.ErrCacheMiss {
-                        sender.Send(msg, 0)
-                        continue
-                }
+		mc_item := &memcache.Item{Key: checksum_str, Value: []byte{1}, Expiration: 300}
 
-                if err == nil {
-                        //fmt.Printf("Dupe %x\n", checksum)
-                } else {
-                        println("Some other error")
-                }
+		if mc_err == memcache.ErrCacheMiss {
+			// A cache miss means that the incoming message is not a dupe.
+			// Set the cache entry to prevent future re-sends of this message.
+			mc.Set(mc_item)
+			// Send the message to subscribers.
+			sender.Send(msg, 0)
+			continue
+		}
 
-        }
+		if mc_err == nil {
+			// No cache miss error means this is a dupe. Just re-set the
+			// cache entry to bump the key expiration time.
+			mc.Set(mc_item)
+			//fmt.Printf("Dupe %x\n", checksum)
+		} else {
+			// Something else unexpected happened.
+			println("MC ERROR:", mc_err.Error())
+		}
+
+	}
 }
