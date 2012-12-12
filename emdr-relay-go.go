bf5ccd1f129953952d15bcb96493b6752cf0db56
Greg Taylor <gtaylor@l11solutions.com>
2012-12-12 00:07:49 -0500

    Committing @temoto's LRU version of the relays. Going to run this on one of the relays to test it out.

--- emdr-relay-go.go
+++ emdr-relay-go.go
@@ -1,16 +1,28 @@
 package main
 
 import (
+    cache "code.google.com/p/vitess/go/cache"
 	"fmt"
 	zmq "github.com/alecthomas/gozmq"
-	"github.com/bradfitz/gomemcache/memcache"
 	"hash"
 	"hash/fnv"
 	"os"
+	"time"
+	"unsafe"
 )
 
+type CacheValue struct {
+	expires time.Time
+}
+
+const cache_value_size = uint64(unsafe.Sizeof(CacheValue{}))
+
+func (self *CacheValue) Size() int {
+	return int(cache_value_size)
+}
+
 func main() {
-	mc := memcache.New("127.0.0.1:11211")
+	cache := cache.NewLRUCache(cache_value_size * 100000)
 
 	context, _ := zmq.NewContext()
 
@@ -23,10 +35,17 @@ func main() {
 	sender, _ := context.NewSocket(zmq.PUB)
 	sender.Bind("tcp://0.0.0.0:8050")
 
+	cache_duration, err := time.ParseDuration("5m")
+    if err != nil {
+        println(err.Error())
+        os.Exit(1)
+    }
+
 	println("Listening on port 8050...")
 
 	for {
 		msg, zmq_err := receiver.Recv(0)
+		now := time.Now()
 
 		if zmq_err != nil {
 			println("RECV ERROR:", zmq_err.Error())
@@ -36,31 +55,29 @@ func main() {
 		h.Write(msg)
 
 		checksum := h.Sum([]byte{})
-		var checksum_str string = fmt.Sprintf("%x", checksum)
+		cache_key := fmt.Sprintf("%x", checksum)
 
-		_, mc_err := mc.Get(checksum_str)
+		cache_item, cache_hit := cache.Get(cache_key)
+		item_expired := false
+		if cache_item != nil {
+			item_expired = now.After(cache_item.(*CacheValue).expires)
+		}
+		cache_item = &CacheValue{expires: now.Add(cache_duration)}
+		// Insert (or reset) the cache entry to prevent future re-sends of this message.
+		cache.Set(cache_key, cache_item)
 
-		mc_item := &memcache.Item{Key: checksum_str, Value: []byte{1}, Expiration: 300}
-
-		if mc_err == memcache.ErrCacheMiss {
+		if !cache_hit {
 			// A cache miss means that the incoming message is not a dupe.
-			// Set the cache entry to prevent future re-sends of this message.
-			mc.Set(mc_item)
 			// Send the message to subscribers.
 			sender.Send(msg, 0)
-			continue
-		}
-
-		if mc_err == nil {
-			// No cache miss error means this is a dupe. Just re-set the
-			// cache entry to bump the key expiration time.
-			mc.Set(mc_item)
-			//fmt.Printf("Dupe %x\n", checksum)
 		} else {
-			// Something else unexpected happened.
-			println("MC ERROR:", mc_err.Error())
-			println(" Checksum:", checksum)
-			os.Exit(1)
+			// Cache hit, dupe if not expired.
+			if !item_expired {
+				// Not expired - dupe.
+			} else {
+				// Expired - valid. Send to subscribers.
+				sender.Send(msg, 0)
+			}
 		}
 
 	}
