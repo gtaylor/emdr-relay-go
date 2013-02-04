fb56e984030fbcba98305fad4d8d5996d0ba8ed4
Greg Taylor <gtaylor@l11solutions.com>
2013-02-04 15:05:19 -0500

    Potential fixes for the CPU issues we see on some relays. Simplified things a good deal as well.

--- emdr-relay-go.go
+++ emdr-relay-go.go
@@ -6,46 +6,40 @@ import (
 	zmq "github.com/alecthomas/gozmq"
 	"hash"
 	"hash/fnv"
-	"os"
-	"time"
 	"unsafe"
 )
 
+// The presence of the cache value is all we need, so keep this super simple.
 type CacheValue struct {
-	expires time.Time
+	found bool
 }
 
+// Calculate the size (in bytes) of our struct.
 const cache_value_size = uint64(unsafe.Sizeof(CacheValue{}))
 
+// Determines the max cache size, in bytes.
+const cache_size_limit = cache_value_size * 1000
+
+// Satisfies the Value interface.
 func (self *CacheValue) Size() int {
 	return int(cache_value_size)
 }
 
 func main() {
-	cache := cache.NewLRUCache(cache_value_size * 1000)
+	cache := cache.NewLRUCache(cache_size_limit)
 
 	context, _ := zmq.NewContext()
-
 	receiver, _ := context.NewSocket(zmq.SUB)
 	receiver.SetSockOptString(zmq.SUBSCRIBE, "")
 	receiver.Connect("tcp://master.eve-emdr.com:8050")
 	receiver.Connect("tcp://secondary.eve-emdr.com:8050")
-	//receiver.Connect("tcp://relay-us-central-1.eve-emdr.com:8050")
-
 	sender, _ := context.NewSocket(zmq.PUB)
 	sender.Bind("tcp://0.0.0.0:8050")
-
-	cache_duration, err := time.ParseDuration("5m")
-	if err != nil {
-		println(err.Error())
-		os.Exit(1)
-	}
 
 	println("Listening on port 8050...")
 
 	for {
 		msg, zmq_err := receiver.Recv(0)
-		now := time.Now()
 
 		if zmq_err != nil {
 			println("RECV ERROR:", zmq_err.Error())
@@ -58,27 +52,19 @@ func main() {
 		cache_key := fmt.Sprintf("%x", checksum)
 
 		cache_item, cache_hit := cache.Get(cache_key)
-		item_expired := false
-		if cache_item != nil {
-			item_expired = now.After(cache_item.(*CacheValue).expires)
+		if cache_hit {
+			// We've already seen this before, ignore it.
+			continue
 		}
-		cache_item = &CacheValue{expires: now.Add(cache_duration)}
-		// Insert (or reset) the cache entry to prevent future re-sends of this message.
+
+		// At this point, we know we've encountered a message we haven't
+		// seen in the recent past.
+		cache_item = &CacheValue{found: true}
+		// Insert the cache entry to prevent future re-sends of this message.
 		cache.Set(cache_key, cache_item)
 
-		if !cache_hit {
-			// A cache miss means that the incoming message is not a dupe.
-			// Send the message to subscribers.
-			sender.Send(msg, 0)
-		} else {
-			// Cache hit, dupe if not expired.
-			if !item_expired {
-				// Not expired - dupe.
-			} else {
-				// Expired - valid. Send to subscribers.
-				sender.Send(msg, 0)
-			}
-		}
-
+		// A cache miss means that the incoming message is not a dupe.
+		// Send the message to subscribers.
+		sender.Send(msg, 0)
 	}
 }
